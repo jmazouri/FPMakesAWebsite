6de4caf4fb1395de4874c1dff25c10ef5dd41019
John Mazouri <jmazouri@users.noreply.github.com>
2015-06-07 11:15:02 -0400

    Woops forgot to make it load more than one page

--- FPMakesAWebsite/HtmlPuller.cs
+++ FPMakesAWebsite/HtmlPuller.cs
@@ -28,7 +28,7 @@ namespace FPMakesAWebsite
         private static void PullNewCode(object sender, ElapsedEventArgs e)
         {
             Console.WriteLine("[{0}] Pulling newest code...", DateTime.Now);
-            CQ doc = CQ.CreateFromUrl("http://facepunch.com/showthread.php?t=1469381", new ServerConfig
+            CQ doc = CQ.CreateFromUrl("http://facepunch.com/showthread.php?t=1469381&goto=newpost", new ServerConfig
             {
                 //I don't think FP likes bots...
                 UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36",
